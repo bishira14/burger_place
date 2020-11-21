23b596573fa7d57bacb39600c53a78bb89e755e6
Hamza Bishira <bishira12@hotmail.co.uk>
2020-11-21 00:50:38 +0000

    connecting to database

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,7 +1,8 @@
 <?php
 
 //connect to database
-$conn = mysqli_connect('localhost', 'uroot', 'uroot12', 'burger_place');
+// $conn = mysqli_connect('localhost', 'uroot', 'uroot12', 'burger_place');
+$conn = mysqli_connect('ivgz2rnl5rh7sphb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 'nqg8w1bhm1sna4h6', 'o1g16w757h23lnml', 'rddrf21ag8s5mv5v');
 
 //check conncection
 if(!$conn){
